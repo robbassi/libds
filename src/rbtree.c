fd315996978eb71f9970c8bed65798a143ac0726
Rob Bassi <rob.bassi.89@gmail.com>
2014-11-10 19:05:51 -0500

    Added rbtree_delete

--- src/rbtree.c
+++ src/rbtree.c
@@ -3,15 +3,15 @@
  *
  *  Created on: Nov 8, 2014
  *      Author: rob
- *      Author: Julienne Walker <http://www.eternallyconfuzzled.com/tuts/datastructures/jsw_tut_rbtree.aspx>
+ *      Author: Julienne Walker <http://www.eternallyconfuzzled.com/tuts/datastructures/rbtree_tut_rbtree.aspx>
  */
 #include <stdlib.h>
 #include "rbtree.h"
 
-rbnode* rbtree_single ( rbnode* root, int dir );
-rbnode* rbtree_double ( rbnode* root, int dir );
+rbnode* rbtree_single(rbnode* root, int dir);
+rbnode* rbtree_double(rbnode* root, int dir);
 void* rbtree_find_r(rbtree* tree, rbnode* root, void* key);
-rbnode* rbtree_insert_r (rbtree* tree, rbnode* root, void* key, void* data);
+//rbnode* rbtree_insert_r(rbtree* tree, rbnode* root, void* key, void* data);
 
 rbtree* rbtree_new(comparator compare) {
 	rbtree* tree = (rbtree*) malloc(sizeof(rbtree));
@@ -42,7 +42,6 @@ void* rbtree_find(rbtree* tree, void* key) {
 		return rbtree_find_r(tree, tree->root->link[dir], key);
 }
 
-
 void* rbtree_find_r(rbtree* tree, rbnode* root, void* key) {
 	puts("recur");
 	if (root == NULL)
@@ -57,44 +56,148 @@ void* rbtree_find_r(rbtree* tree, rbnode* root, void* key) {
 		return rbtree_find_r(tree, root->link[dir], key);
 }
 
-void rbtree_insert ( rbtree* tree, void* key, void* data) {
-  tree->root = rbtree_insert_r ( tree, tree->root, key, data );
-  tree->root->color = BLACK;
+void rbtree_insert(rbtree*tree, void* key, void* data) {
+	if (tree->root == NULL) {
+		/* Empty tree case */
+		tree->root = rbnode_new(key, data);
+		if (tree->root == NULL)
+			return;
+	} else {
+		rbnode head = { BLACK }; /* False tree root */
+
+		rbnode *g, *t; /* Grandparent & parent */
+		rbnode *p, *q; /* Iterator & parent */
+		int dir = 0, last;
+
+		/* Set up helpers */
+		t = &head;
+		g = p = NULL;
+		q = t->link[1] = tree->root;
+
+		/* Search down the tree */
+		for (;;) {
+			if (q == NULL) {
+				/* Insert new node at the bottom */
+				p->link[dir] = q = rbnode_new(key, data);
+				if (q == NULL)
+					return;
+			} else if (is_red(q->link[0]) && is_red(q->link[1])) {
+				/* Color flip */
+				q->color = RED;
+				q->link[0]->color = BLACK;
+				q->link[1]->color = BLACK;
+			}
+
+			/* Fix red violation */
+			if (is_red(q) && is_red(p)) {
+				int dir2 = t->link[1] == g;
+
+				if (q == p->link[last])
+					t->link[dir2] = rbtree_single(g, !last);
+				else
+					t->link[dir2] = rbtree_double(g, !last);
+			}
+
+			/* Stop if found */
+			if (tree->compare(q->key, key) == 0)
+				break;
+
+			last = dir;
+			dir = tree->compare(q->key, key) < 0;
+
+			/* Update helpers */
+			if (g != NULL)
+				t = g;
+			g = p, p = q;
+			q = q->link[dir];
+		}
+
+		/* Update root */
+		tree->root = head.link[1];
+	}
+
+	/* Make root black */
+	tree->root->color = BLACK;
 }
 
-rbnode* rbtree_insert_r (rbtree* tree, rbnode* root, void* key, void* data) {
-  if ( root == NULL )
-    root = rbnode_new ( key, data );
-  else if (tree->compare(root->key, key) != 0) {
-    int dir = tree->compare(root->key, key) < 0;
+int rbtree_remove(rbtree *tree, void* key) {
+	if (tree->root != NULL) {
+		rbnode head = { 0 }; /* False tree root */
+		rbnode *q, *p, *g; /* Helpers */
+		rbnode *f = NULL; /* Found item */
+		int dir = 1;
 
-    root->link[dir] = rbtree_insert_r ( tree, root->link[dir], key, data );
+		/* Set up helpers */
+		q = &head;
+		g = p = NULL;
+		q->link[1] = tree->root;
 
-    if ( is_red ( root->link[dir] ) ) {
-      if ( is_red ( root->link[!dir] ) ) {
-        /* Case 1 */
-        root->color = RED;
-        root->link[0]->color = BLACK;
-        root->link[1]->color = BLACK;
-      }
-      else {
-        /* Cases 2 & 3 */
-        if ( is_red ( root->link[dir]->link[dir] ) )
-          root = rbtree_single ( root, !dir );
-        else if ( is_red ( root->link[dir]->link[!dir] ) )
-          root = rbtree_double ( root, !dir );
-      }
-    }
-  }
+		/* Search and push a red down */
+		while (q->link[dir] != NULL) {
+			int last = dir;
 
-  return root;
+			/* Update helpers */
+			g = p, p = q;
+			q = q->link[dir];
+			dir = tree->compare(q->key, key) < 0;
+
+			/* Save found node */
+			if (tree->compare(q->key, key) == 0)
+				f = q;
+
+			/* Push the red node down */
+			if (!is_red(q) && !is_red(q->link[dir])) {
+				if (is_red(q->link[!dir]))
+					p = p->link[last] = rbtree_single(q, dir);
+				else if (!is_red(q->link[!dir])) {
+					rbnode *s = p->link[!last];
+
+					if (s != NULL) {
+						if (!is_red(s->link[!last]) && !is_red(s->link[last])) {
+							/* Color flip */
+							p->color = BLACK;
+							s->color = RED;
+							q->color = RED;
+						} else {
+							int dir2 = g->link[1] == p;
+
+							if (is_red(s->link[last]))
+								g->link[dir2] = rbtree_double(p, last);
+							else if (is_red(s->link[!last]))
+								g->link[dir2] = rbtree_single(p, last);
+
+							/* Ensure correct coloring */
+							q->color = g->link[dir2]->color = RED;
+							g->link[dir2]->link[0]->color = BLACK;
+							g->link[dir2]->link[1]->color = BLACK;
+						}
+					}
+				}
+			}
+		}
+
+		/* Replace and remove if found */
+		if (f != NULL) {
+			f->key = q->key;
+			f->data = q->data;
+			p->link[p->link[1] == q] = q->link[q->link[0] == NULL];
+			free(q);
+		}
+
+		/* Update root and make it black */
+		tree->root = head.link[1];
+		if (tree->root != NULL)
+			tree->root->color = BLACK;
+	}
+
+	return 1;
 }
 
-int is_red (rbnode* root) {
+int is_red(rbnode* root) {
 	return root != NULL && root->color == RED;
 }
 
-rbnode* rbtree_single ( rbnode* root, int dir ) {
+rbnode* rbtree_single(rbnode* root, int dir) {
 	puts("single");
 	rbnode* save = root->link[!dir];
 
@@ -105,51 +208,51 @@ rbnode* rbtree_single ( rbnode* root, int dir ) {
 	save->color = BLACK;
 
 	return save;
- }
-
-rbnode* rbtree_double ( rbnode* root, int dir ) {
-	puts("double");
-	root->link[!dir] = rbtree_single ( root->link[!dir], !dir );
-	return rbtree_single ( root, dir );
 }
 
-int rbtree_rb_assert ( rbnode* root ) {
+rbnode* rbtree_double(rbnode* root, int dir) {
+	puts("double");
+	root->link[!dir] = rbtree_single(root->link[!dir], !dir);
+	return rbtree_single(root, dir);
+}
+
+int rbtree_rb_assert(rbnode* root) {
 	int lh, rh;
 
-	if ( root == NULL )
+	if (root == NULL)
 		return 1;
 	else {
 		rbnode* ln = root->link[0];
 		rbnode* rn = root->link[1];
 
-	/* Consecutive red links */
-	if ( is_red ( root ) ) {
-		if ( is_red ( ln ) || is_red ( rn ) ) {
-			puts ( "Red violation" );
+		/* Consecutive red links */
+		if (is_red(root)) {
+			if (is_red(ln) || is_red(rn)) {
+				puts("Red violation");
+				return 0;
+			}
+		}
+
+		lh = rbtree_rb_assert(ln);
+		rh = rbtree_rb_assert(rn);
+
+		/* Invalid binary search tree */
+		if ((ln != NULL && ln->data >= root->data)
+				|| (rn != NULL && rn->data <= root->data)) {
+			puts("Binary tree violation");
 			return 0;
 		}
-	}
 
-	lh = rbtree_rb_assert ( ln );
-	rh = rbtree_rb_assert ( rn );
+		/* Black height mismatch */
+		if (lh != 0 && rh != 0 && lh != rh) {
+			puts("Black violation");
+			return 0;
+		}
 
-	/* Invalid binary search tree */
-	if ( ( ln != NULL && ln->data >= root->data )
-			|| ( rn != NULL && rn->data <= root->data ) ) {
-		puts ( "Binary tree violation" );
-		return 0;
-	}
-
-	/* Black height mismatch */
-	if ( lh != 0 && rh != 0 && lh != rh ) {
-		puts ( "Black violation" );
-		return 0;
-	}
-
-	/* Only count black links */
-	if ( lh != 0 && rh != 0 )
-		return is_red ( root ) ? lh : lh + 1;
-	else
-		return 0;
+		/* Only count black links */
+		if (lh != 0 && rh != 0)
+			return is_red(root) ? lh : lh + 1;
+		else
+			return 0;
 	}
 }
