f1ae92be8c75560a68c58d5299ade27be3bf11f6
Rob Bassi <rob.bassi.89@gmail.com>
2014-11-14 21:51:32 -0500

    Experimental hashmap implementations

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Tests
 
-test: test_hashmap test_rbtree
+test: test_hashmap test_rbtree test_hashhashmap test_oa_hashmap
 
 test_hashmap: hashmap
 	gcc -o test_hashmap tests/test_hashmap.c hashmap.o
@@ -11,13 +11,23 @@ test_rbtree: rbtree
 	gcc -o test_rbtree tests/test_rbtree.c rbtree.o
 	./test_rbtree
 	rm test_rbtree
+	
+test_hashhashmap: hashhashmap
+	gcc -o test_hashhashmap tests/test_hashhashmap.c hashhashmap.o
+	./test_hashhashmap
+	rm test_hashhashmap
 
 gnuplot: libs/gnuplot_i.c libs/gnuplot_i.h
 	$(CC) $(CFLAGS) -c -o gnuplot_i.o libs/gnuplot_i.c
+
+test_oa_hashmap: oa_hashmap gnuplot
+	gcc -o test_oa_hashmap tests/test_oa_hashmap.c oa_hashmap.o gnuplot_i.o
+	./test_oa_hashmap
+	rm test_oa_hashmap
 	
 # Core
 
-all: hashmap rbtree
+all: hashmap rbtree hashhashmap oa_hashmap
 	
 hashmap:
 	gcc -c -o hashmap.o src/hashmaps/hashmap.c
@@ -25,5 +35,11 @@ hashmap:
 rbtree:
 	gcc -c -o rbtree.o src/trees/rbtree.c	
 
+hashhashmap:
+	gcc -c -o hashhashmap.o src/hashmaps/hashhashmap.c
+
+oa_hashmap:
+	gcc -c -o oa_hashmap.o src/hashmaps/open_address.c
+	
 clean:
 	rm *.o
---
+++ tests/test_hashhashmap.c
@@ -0,0 +1,77 @@
+/*
+ * test_hashhashmap.c
+ *
+ *  Created on: Nov 8, 2014
+ *      Author: rob
+ */
+
+#include <time.h>
+#include <stdio.h>
+#include "fixtures.h"
+#include "../src/hashmaps/hashhashmap.h"
+
+void hashmap_display(hashmap* map) {
+	int i;
+	for (i = 0; i < map->size; i++) {
+		entry* cursor = map->entries[i];
+		printf("%d: \n", i);
+		while (cursor != NULL) {
+			printf("\tkey: %s, value: %d\n", cursor->key, *(int*)cursor->data);
+			cursor = cursor->next;
+		}
+	}
+}
+
+void hashhashmap_display(hashhashmap* map) {
+	hashmap* map2;
+	int i;
+	for (i = 0; i < map->size; i++) {
+		map2 = map->hashmaps[i];
+		if (map2) {
+			printf("%d:\n", i);
+			hashmap_display(map2);
+		} else {
+			printf("%d: NULL\n", i);
+		}
+	}
+}
+
+int main(int argc, char**argv) {
+
+	hashhashmap* map = hashhashmap_new(15, hash1, hash2);
+
+	int size = 10000;
+	int i;
+	test_entry* test_entries = malloc(size * sizeof(test_entry));
+	test_entries = get_test_data(size);
+
+	print_divider();
+	printf("Hashhashmap[%d]\n", size);
+
+	clock_t start = clock(), diff;
+	for(i=0; i<size; i++) {
+		hashhashmap_put(map, test_entries[i].key, &test_entries[i].data);
+	}
+	diff = clock() - start;
+	printf("PUT: \t\t%gs\n", (double)diff/(double)CLOCKS_PER_SEC);
+
+	for(i=0; i<size; i++) {
+		hashhashmap_get(map, test_entries[i].key);
+	}
+
+
+	diff = clock() - diff;
+	printf("GET: \t\t%gs\n", (double)diff/(double)CLOCKS_PER_SEC);
+
+	for(i=0; i<size; i++) {
+		hashhashmap_delete(map, test_entries[i].key);
+	}
+
+	diff = clock() - diff;
+	printf("DELETE: \t%gs\n", (double)diff/(double)CLOCKS_PER_SEC);
+
+	//hashhashmap_display(map);
+	print_divider();
+
+	return 0;
+}
